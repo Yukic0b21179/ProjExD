29e417d53910112fffdeb27b1eee6e659c82e0bd
Yukic0b21179 <c0b211790f@edu.teu.ac.jp>
2022-06-21 15:53:07 +0900

    計算機

---
+++ ex02/calc.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+from winreg import REG_RESOURCE_REQUIREMENTS_LIST
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    if num == "=":
+        eqn = entry.get()
+        result = eval(eqn)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END,result)
+    else:
+        entry.insert(tk.END,num)
+
+    # tkm.showinfo("", f"{num}のボタンがクリックされました")
+    # entry.insert(tk.END, num)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("超高機能電卓")
+    # root.geometry("300x500")
+
+    entry = tk.Entry(root,justify = "right",width = 10,font = ("Times New Roman", 40))
+    entry.grid(row = 0,column = 0,columnspan = 3, ) #横方向に３マス結合
+
+r, c = 1, 0 #r:行番号　c:列番号
+
+list = [9,8,7,6,5,4,3,2,1,0,"+","="]
+
+for num in list:
+    btn = tk.Button(root,
+                    text = f"{num}",
+                    width = 4,
+                    height = 2,
+                    font = ("Times New Roman", 30)
+                    )
+    btn.bind("<1>",button_click)
+    btn.grid(row = r, column = c)
+
+    c += 1
+
+    if isinstance(num, int):
+        if (num-1)%3 == 0:
+            r += 1
+            c = 0
+
+root.mainloop()
