6a5c56bdff91462bd8a49d70d15314ddfc093fce
Yukic0b21179 <c0b211790f@edu.teu.ac.jp>
2022-06-28 17:44:49 +0900

    追加機能

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,16 +2,21 @@ from tokenize import maybe
 import maze_maker
 import tkinter as tk
 
-def key_down(event):
+def count_down():     #カウントダウン関数　一秒ごとにtmrのカウントを-1する。
+    global tmr
+    tmr -= 1
+    canvas.create_text(400, 50, text = f"制限時間：{tmr}", font = ("Times New Roman", 80), tag = "制限時間")
+
+def key_down(event):    #キーが押されたら
     global key 
     key = event.keysym
 
-def key_up(event):
+def key_up(event):      #キーが離されたら
     global key 
     key = ""
 
-def main_proc():
-    global cx,cy,key,mx,my
+def main_proc():    #キャラクターの座標の関する処理
+    global cx,cy,key,mx,my,cnt
 
     if key != "":
 
@@ -24,18 +29,30 @@ def main_proc():
 
         mx, my = mx + delta[key][0], my + delta[key][1]
     
-        if list[my][mx] == 0:
+        if list[my][mx] == 0:   #マスのリストが床判定だったら
 
             cx, cy = mx*100 + 50, my*100 +50
 
             canvas.coords("tori", cx, cy)
 
         else:
-            mx, my = mx - delta[key][0], my - delta[key][1]
+            mx, my = 1, 1
+            cx, cy = mx*100 + 50, my*100 +50
+            canvas.delete("tori")
+            canvas.create_image(cx, cy, image = tori, tag = "tori")
+
+    cnt += 1
             
+    if cnt == 10:   #一秒後
+        canvas.delete("制限時間")
+        cnt = 1
+        count_down()
+
     root.after(100, main_proc)
 
 if __name__ == "__main__":
+
+    cnt = 1
 
     mx, my = 1, 1
 
@@ -49,6 +66,7 @@ if __name__ == "__main__":
     maze_maker.show_maze(canvas,list)
 
     tori = tk.PhotoImage(file = "fig/6.png")
+    tori2 = tk.PhotoImage(file = "fig/6.png")
 
     cx, cy = mx * 100 + 50,my * 100 + 50
     canvas.create_image(cx, cy, image = tori, tag = "tori")
@@ -60,5 +78,10 @@ if __name__ == "__main__":
     main_proc()
 
     jid = 0
+
+    # label = tk.Label(root, text="制限時間", font = ("Times New Roman", 80))
+    # label.pack()
+    tmr = 100
+    count_down()
 
     root.mainloop()
