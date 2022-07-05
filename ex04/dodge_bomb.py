d1b733a9de553a2eb00f677545638a0cf738afcf
Yukic0b21179 <c0b211790f@edu.teu.ac.jp>
2022-07-05 17:20:17 +0900

    追加機能

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,9 +1,10 @@
 import pygame as pg
 import sys
 import random
+import tkinter as tk
 
 def main():
-
+    global cnt
     clock = pg.time.Clock()
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1280, 720))   #Sarface ×ボタンが出なくなるのでサイズ変更
@@ -12,7 +13,7 @@ def main():
     bgimg_rct = bgimg_sfc.get_rect()                #Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
-    kkimg_sfc = pg.image.load("fig/6.png")          #Sarface
+    kkimg_sfc = pg.image.load(f"fig/{cnt}.png")          #Sarface 鳥の画像変更
     kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
     kkimg_rct = kkimg_sfc.get_rect()                #Rect
     kkimg_rct.center = 900, 400
@@ -40,13 +41,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        key_states = pg.key.get_pressed()
+        key_states = pg.key.get_pressed()       #キーが押されたとき
         if key_states[pg.K_UP] == True: kkimg_rct.centery -= 1
         if key_states[pg.K_DOWN] == True: kkimg_rct.centery += 1
         if key_states[pg.K_LEFT] == True: kkimg_rct.centerx -= 1
         if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
 
-        if check_bound(kkimg_rct, screen_rct) != (1, 1):
+        if check_bound(kkimg_rct, screen_rct) != (1, 1):    #チェック関数が通らなかったとき
             if key_states[pg.K_UP] == True: kkimg_rct.centery += 1
             if key_states[pg.K_DOWN] == True: kkimg_rct.centery -= 1
             if key_states[pg.K_LEFT] == True: kkimg_rct.centerx += 1
@@ -55,7 +56,7 @@ def main():
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
         bomb_rct.move_ip(vx, vy)
-        ret = check_bound(bomb_rct, screen_rct)
+        ret = check_bound(bomb_rct, screen_rct) #x,y座標がスクリーン画面から超えているかどうか、超えている場合は-1をかける
         vx = vx*ret[0]
         vy = vy*ret[1]
 
@@ -65,10 +66,23 @@ def main():
 
         clock.tick(1000)
 
-        if kkimg_rct.colliderect(bomb_rct): return
+        if kkimg_rct.colliderect(bomb_rct): break
+
+    global root
+    root = tk.Tk()                                                              #gameover画面(y/n)yの場合コンティニュー、nの場合ゲームを終了
+    root.geometry("220x100")
+    root.title("GameOver")
+    label = tk.Label(root, text="continue?", font = ("Times New Roman", 40))
+    label.place(y = 90)
+    label.pack()
+    btn1 = tk.Button(root, text = "Yes", command = Main)
+    btn1.place(x = 63, y = 60)
+    btn2 = tk.Button(root, text = "No", command = exit)
+    btn2.place(x = 126, y = 60)
+    root.mainloop()
             
 
-def check_bound(rect, screen):
+def check_bound(rect, screen):  #スクリーンから出ていない場合は1出ている場合は-1を返す
     a, b = 1, 1
     if rect.left < screen.left or screen.right < rect.right:
         a = -1
@@ -77,8 +91,23 @@ def check_bound(rect, screen):
 
     return a, b
 
-if __name__ == "__main__":
-    pg.init()
-    main()
+root = None
+def exit():
     pg.quit()
     sys.exit()
+
+def Main():     #鳥のカウントとゲームオーバー画面の非表示
+    global root,cnt
+    cnt += 1
+    if cnt == 10:
+        cnt = 0
+    root.destroy()
+    main()
+
+if __name__ == "__main__":
+    cnt = 0
+    pg.init()
+    main()
+
+
+    
